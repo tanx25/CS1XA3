7521262b11bb5d1722a9cb7da8dd8b3b3b600b54
Frank <tanx25@mcmaster.ca>
2020-02-29 01:43:14 +0000

    Submitting Project 01 part 2

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
 if [ "$1" = "1" ]; then 
-	./feature1
-fi
-if [ "$1" = "2" ]; then 
-        ./feature2
+	./feature2
 fi
 if [ "$1" = "2" ]; then 
         ./feature3
 fi
-if [ "$1" = "2" ]; then 
+if [ "$1" = "3" ]; then 
         ./feature4
 fi
-if [ "$1" = "2" ]; then 
+if [ "$1" = "4" ]; then 
         ./feature5
 fi
-if [ "$1" = "2" ]; then 
+if [ "$1" = "5" ]; then 
         ./feature6
 fi
-if [ "$1" = "2" ]; then 
+if [ "$1" = "6" ]; then 
         ./feature7
 fi
-if [ "$1" = "2" ]; then 
+if [ "$1" = "7" ]; then 
         ./feature8
 fi
 
